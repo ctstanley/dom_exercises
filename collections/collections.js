7269567a55ab1e542d6cb67714188b3f5d150ac6
ctstanley <ctstanley1@hotmail.com>
2015-04-30 20:29:21 -0700

    my homework

--- collections/collections.js
+++ collections/collections.js
@@ -1,18 +1,30 @@
 //
 // Part 1
 //
+var bord = document.querySelectorAll("li")
 
-
-
+for (var i = 0; i < bord.length; i++) {
+	var borderCh = bord[i];
+	borderCh.style.borderBottom = '2px solid black';
+};
 
 //
 // Part 2
 //
+var redRum = document.querySelectorAll(".datatype");
 
-
+for(var i =0; i < redRum.length; i++) {
+	var red = redRum[i];
+	red.style.color = "red";
+}
 
 
 //
 // Part 3
 //
+var funBlue = document.querySelectorAll(".function");
 
+for(var i =0; i < funBlue.length; i++) {
+	var blue = funBlue[i];
+	blue.style.color = "blue";
+}
--- events/events.js
+++ events/events.js
@@ -1,1 +1,56 @@
 // Your code here
+window.addEventListener("load", function () {
+	
+	var navi = document.querySelector("#clicker");
+
+	navi.addEventListener("click", function () {
+		alert("Congratulations! You are a clicker!");
+		console.log(event.target);
+	});
+
+	var hey = document.querySelector("#mouser");
+
+	hey.addEventListener("mouseover", function () {
+		hey.style.backgroundColor = "yellow";
+		console.log(event.target);
+	});
+
+	var listen = document.querySelector("#mouser");
+
+	listen.addEventListener("mouseout", function () {
+		listen.style.backgroundColor = "green";
+		console.log(event.target);
+	});
+
+	var link = document.querySelector("#former");
+
+	link.addEventListener("submit", function () {
+		alert(link.term.value);
+		console.log(event.target);
+		event.preventDefault();
+	});
+//If prevent default is not used, the form is submitted
+//to the .html (ie. .html?term='input').
+	var hyrule = document.querySelector("#vegetables");
+
+	hyrule.addEventListener("click", function () {
+		alert(event.target.innerText);
+		console.log(event.target.innerText);
+	})
+///Cannot read property 'addEventListener' of null, the 
+///script is not in the right node.
+	console.log(event.target);
+
+})
+
+//1.) If prevent default is not used, the form is submitted
+//to the .html (ie. .html?term='input').
+//2.) We are getting the input value.
+//3.) Load the script that we need to use in order fo the
+//code to be executed.
+
+
+
+
+
+	
